c16a13f43bd9f29c75cf9e8b97e7ef851b84b7ff
arath11 <julioarath@gmail.com>
2020-09-02 01:27:51 +0000

    Practica 2

--- xv6-public/kill.c
+++ xv6-public/kill.c
@@ -6,12 +6,22 @@ int
 main(int argc, char **argv)
 {
   int i;
+    if(argc==2 ){
+      
+      
+    }
 
   if(argc < 2){
     printf(2, "usage: kill pid...\n");
     exit();
   }
+
   for(i=1; i<argc; i++)
-    kill(atoi(argv[i]));
+     if(atoi(argv[i])==1){
+        printf(1, "A mi no me matas \n");
+//        exit();
+    }else {
+        kill(atoi(argv[i]));
+    }
   exit();
 }
