3140fd66f3446ac820128b2973c7043a8eb55ddd
arath11 <julioarath@gmail.com>
2020-11-11 03:21:08 +0000

    Practica 9

--- practicas/9/copy.c
+++ practicas/9/copy.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv){
     char *sourceFile = argv[1];
     char *destinationFile = argv[2];
 
-    int fd=open("./lee.c",O_RDONLY,0); // abrir archivos fopen 
+    int fd=open(sourceFile,O_RDONLY,0); // abrir archivos fopen 
     if(fd<0){
         printf("Error al abrir el archivo ");
         return 1;
